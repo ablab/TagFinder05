c5ca762b3a99d7490257c29e9bdd67c9258f284c
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-12-28 15:26:59 +0400

    Just commit the last version before release. http://ablab.myjetbrains.com/youtrack/issue/TagFinder-40

--- src/ru/spbau/bioinf/tagfinder/FourSearchPositive.java
+++ src/ru/spbau/bioinf/tagfinder/FourSearchPositive.java
@@ -20,6 +20,8 @@ public class FourSearchPositive {
         df.setMaximumFractionDigits(2);
     }
 
+    static int TAG_SIZE = 4;
+    
     public static void main(String[] args) throws Exception {
         conf = new Configuration(args);
         EValueAdapter.init(conf);
@@ -51,11 +53,12 @@ public class FourSearchPositive {
         int good = 0;
         int bad = 0;
 
-        int TAG_SIZE = 3;
-
         for (int scanId : keys) {
-            if (!forProcess.contains(scanId)) {
+            if (msAlignResults.containsKey(scanId)) {
                 continue;
+            }
+            if (!forProcess.contains(scanId)) {
+                //continue;
             }
             Scan scan = scans.get(scanId);
             List<Peak> peaks = scan.createSpectrumWithYPeaks(PrecursorMassShiftFinder.getPrecursorMassShiftForMoreEdges(conf, scan));
@@ -64,59 +67,8 @@ public class FourSearchPositive {
             GraphUtil.generateGapEdges(conf, peaks, 1);
             List<List<Peak>> componentsFromGraph = GraphUtil.getComponentsFromGraph(peaks);
             final Map<Integer, Integer> score = new HashMap<Integer, Integer>();
-            System.out.println("Scan " + scanId) ;
-            for (List<Peak> component : componentsFromGraph) {
-                Peak[] tagPeaks = GraphUtil.findBestTag(component);/*
-                int len = tagPeaks.length;
-                if (len > 5) {
-                    Collections.sort(component, new Comparator<Peak>() {
-                        public int compare(Peak o1, Peak o2) {
-                            double intensity1 = o1.getIntensity();
-                            double intensity2 = o2.getIntensity();
-                            if (intensity1 == intensity2) {
-                                return 0;
-                            }
-                            return intensity2 - intensity1 > 0 ? 1 : -1;
-                        }
-                    });
-                    while (true) {
-                        component.remove(component.size() - 1);
-                        for (Peak peak : component) {
-                            peak.clearEdges();
-                        }
-                        GraphUtil.generateEdges(conf, peaks);
-                        Peak[] tagPeaks2 = GraphUtil.findBestTag(component);
-                        if (tagPeaks2.length == tagPeaks.length) {
-                            tagPeaks = tagPeaks2;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                */
-                String superTag = "";
-                if (tagPeaks.length < TAG_SIZE  + 1) {
-                    continue;
-                }
-                for (int j = 0; j < tagPeaks.length  - 1; j++) {
-                    superTag += Acid.getAcid(tagPeaks[j + 1].getValue() - tagPeaks[j].getValue()).name();
-                }
-                //superTag = "AVSFMV";
-                //System.out.print(superTag + " ");
-
-                for (int i = 0; i < superTag.length() - TAG_SIZE + 1; i++) {
-                    String tag = superTag.substring(i, i + TAG_SIZE);
-                    for (Protein protein : proteins) {
-                        if (protein.getSimplifiedAcids().contains(tag)) {
-                            int proteinId = protein.getProteinId();
-                            if (!score.containsKey(proteinId)) {
-                                score.put(proteinId, 0);
-                            }
-                            score.put(proteinId, score.get(proteinId) + 1);
-                        }
-                    }
-                }
-            }
+            //System.out.println("Scan " + scanId) ;
+            fillScoresSeparator(componentsFromGraph, score);
             if (!score.isEmpty()) {
                 List<Integer> proteinIds = new ArrayList<Integer>();
                 proteinIds.addAll(score.keySet());
@@ -132,19 +84,37 @@ public class FourSearchPositive {
                     }
                     int proteinId = proteinIds.get(i);
                     if (score.get(proteinId) > 3) {
-                        //System.out.println(proteinId + " " + score.get(proteinId) + " " + proteins.get(proteinId).getName());
-                        if (msAlignResults.get(scanId) == proteinId) {
-                            hasGood = true;
+                        Protein protein = proteins.get(proteinId);
+                        String name = protein.getName();
+                        name = name.substring(0, name.lastIndexOf("|"));
+                        name = name.substring(name.lastIndexOf("|") + 1);
+                        String seq = protein.getSimplifiedAcids();
+
+                        String tagsList = "";
+                        for (int size = 20; size > 3; size--) {
+                            for (String tag : getTags(componentsFromGraph)) {
+                                for (int s = 0; s < tag.length() - size; s++) {
+                                    String subtag = tag.substring(s, s + size);
+                                    if (seq.contains(subtag) && !tagsList.contains(subtag)) {
+                                        tagsList += " " + subtag;
+                                    }
+                                }
+                            }
+                            //if (msAlignResults.get(scanId) == proteinId) {
+                            //    hasGood = true;
+                            //}
                         }
+                        System.out.println(scanId + " " + name + " " + proteinId + " " + score.get(proteinId) + " " + EValueAdapter.getBestEValue(scan, proteinId) + " " + tagsList);
                     }
+                }
                     /*
                     double evalue = EValueAdapter.getBestEValue(scan, proteinId);
                     if (evalue < 100) {
                         System.out.println();
                         System.out.println(evalue);
                     }
-                    */
-                }
+
+                    }
                 if (hasGood) {
                     good++;
                 } else {
@@ -154,6 +124,109 @@ public class FourSearchPositive {
                 bad++;
             }
             System.out.println("good " + good + " bad " + bad);
+               */
+            }
         }
+    }
+
+    private static void fillScoresOriginal(List<List<Peak>> componentsFromGraph, Map<Integer, Integer> score) {        
+        for (List<Peak> component : componentsFromGraph) {
+            Peak[] tagPeaks = GraphUtil.findBestTag(component);/*
+            int len = tagPeaks.length;
+            if (len > 5) {
+                Collections.sort(component, new Comparator<Peak>() {
+                    public int compare(Peak o1, Peak o2) {
+                        double intensity1 = o1.getIntensity();
+                        double intensity2 = o2.getIntensity();
+                        if (intensity1 == intensity2) {
+                            return 0;
+                        }
+                        return intensity2 - intensity1 > 0 ? 1 : -1;
+                    }
+                });
+                while (true) {
+                    component.remove(component.size() - 1);
+                    for (Peak peak : component) {
+                        peak.clearEdges();
+                    }
+                    GraphUtil.generateEdges(conf, peaks);
+                    Peak[] tagPeaks2 = GraphUtil.findBestTag(component);
+                    if (tagPeaks2.length == tagPeaks.length) {
+                        tagPeaks = tagPeaks2;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            */
+            String superTag = "";
+            if (tagPeaks.length < TAG_SIZE  + 1) {
+                continue;
+            }
+            for (int j = 0; j < tagPeaks.length  - 1; j++) {
+                superTag += Acid.getAcid(tagPeaks[j + 1].getValue() - tagPeaks[j].getValue()).name();
+            }
+            //superTag = "AVSFMV";
+            //System.out.print(superTag + " ");
+
+            for (int i = 0; i < superTag.length() - TAG_SIZE + 1; i++) {
+                String tag = superTag.substring(i, i + TAG_SIZE);
+                for (Protein protein : proteins) {
+                    if (protein.getSimplifiedAcids().contains(tag)) {
+                        int proteinId = protein.getProteinId();
+                        if (!score.containsKey(proteinId)) {
+                            score.put(proteinId, 0);
+                        }
+                        score.put(proteinId, score.get(proteinId) + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fillScoresSeparator(List<List<Peak>> componentsFromGraph, Map<Integer, Integer> scores) {
+        List<String> tags = getTags(componentsFromGraph);
+        int subSequenceLength = 300;        
+        for (Protein protein : proteins) {
+            int score = 0;
+            String sequence = protein.getSimplifiedAcids();
+            int end = sequence.length() - subSequenceLength + 1;
+            if (end < 1) {
+                end = 1;
+            }
+            for (int start = 0; start < end; start++) {
+                String s = sequence.substring(start, Math.min(start + subSequenceLength, sequence.length()));
+                int nextScore = 0;
+                for (String tag : tags) {
+                    for (int i = 0; i < tag.length() - TAG_SIZE; i++) {
+                        if (s.contains(tag.substring(i, i + TAG_SIZE))) {
+                            nextScore++;
+                        }
+                    }
+                }
+                if (nextScore > score) {
+                    score = nextScore;
+                }
+            }
+            if (score > 0) {
+                scores.put(protein.getProteinId(), score);
+            }
+        }
+    }
+
+    private static List<String> getTags(List<List<Peak>> componentsFromGraph) {
+        List<String> tags = new ArrayList<String>();
+        for (List<Peak> component : componentsFromGraph) {
+            Peak[] tagPeaks = GraphUtil.findBestTag(component);
+            String superTag = "";
+            if (tagPeaks.length < TAG_SIZE  + 1) {
+                continue;
+            }
+            for (int j = 0; j < tagPeaks.length  - 1; j++) {
+                superTag += Acid.getAcid(tagPeaks[j + 1].getValue() - tagPeaks[j].getValue()).name();
+            }
+            tags.add(superTag);
+        }
+        return tags;
     }
 }

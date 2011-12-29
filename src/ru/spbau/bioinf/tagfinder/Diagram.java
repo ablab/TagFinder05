fa8baeaafe4b0920d8a1577b79b5dade6ec2160c
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-12-29 16:15:18 +0400

    Read search results from text files.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-40

--- src/ru/spbau/bioinf/tagfinder/Diagram.java
+++ src/ru/spbau/bioinf/tagfinder/Diagram.java
@@ -2,6 +2,7 @@ package ru.spbau.bioinf.tagfinder;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -13,36 +14,63 @@ import ru.spbau.bioinf.tagfinder.util.ReaderUtil;
 public class Diagram {
     public static void main(String[] args) throws Exception {
         Set<Integer> matchedNew = new HashSet<Integer>();
-        BufferedReader in = ReaderUtil.createInputReader(new File("tag4first.log"));
-        String s;
-        while ((s = in.readLine())!= null) {
-            if (!s.startsWith("#")) {
-                String[] data = s.split(" ");
-                if (data.length > 1) {
-                    if (Double.parseDouble(data[2]) < 0.0024) {
-                        matchedNew.add(Integer.parseInt(data[0]));
+        File dir = new File("");
+        dir = new File(dir.getCanonicalPath());
+        File[] txtFiles = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".txt");
+            }
+        });
+        int lines = 0;
+        for (File txt : txtFiles) {
+            BufferedReader in = ReaderUtil.createInputReader(txt);
+            String s = in.readLine();
+            if (!s.startsWith("#Total ")) {
+                continue;
+            }
+            System.out.println("Processing file " + txt.getName());
+
+            while ((s = in.readLine())!= null) {
+                if (!s.startsWith("#")) {
+                    lines++;
+                    String[] data = s.split(" ");
+                    if (data.length > 1) {
+                        if (Double.parseDouble(data[2]) < 0.0024) {
+                            matchedNew.add(Integer.parseInt(data[0]));
+                        }
                     }
                 }
             }
         }
+        System.out.println(lines + " results processed.");
+        BufferedReader readerMsalign = ReaderUtil.createInputReader(new File("msalign.txt"));
+        Set<Integer> msAlignOld = new HashSet<Integer>();
+        String s;
+        while ((s = readerMsalign.readLine())!= null) {
+            msAlignOld.add(Integer.parseInt(s));
+        }
+        
         Configuration conf = new Configuration(args);
         Map<Integer, Scan> scans = conf.getScans();
-        Map<Integer, Integer> msAlignResults = conf.getMSAlignResults();
-        Map<Integer, Double> evalues = conf.getEvalues();
+        List<Integer> keys = new ArrayList<Integer>();
+        for (Scan scan : scans.values()) {
+            if (scan.getPeaks().size() >= 10 && scan.getPrecursorMass() > 2500) {
+                keys.add(scan.getId());
+            }
+        }
+        Collections.sort(keys);
+
+
         int msAlign = 0;
         int tag = 0;
         int both = 0;
         int total = 0;
         int tagOnly = 0;
         int msOnly = 0;
-        List<Integer> keys = new ArrayList<Integer> ();
-        keys.addAll(msAlignResults.keySet());
-        Collections.sort(keys);
         for (int scanId : keys) {
             total++;
-            Scan scan = scans.get(scanId);
             boolean isNew = matchedNew.contains(scanId);
-            boolean isOld = evalues.get(scanId) < 0.0024;
+            boolean isOld = msAlignOld.contains(scanId);
             if (isOld) {
                 //System.out.println(scanId);
             }

41566df3e3caafab833ae533519495bb57e7ec08
Yakov Sirotkin <Yakov Sirotkin@.(none)>
2011-12-29 13:02:04 +0400

    New diagram generator.  http://ablab.myjetbrains.com/youtrack/issue/TagFinder-40

--- src/ru/spbau/bioinf/tagfinder/Diagram.java
+++ src/ru/spbau/bioinf/tagfinder/Diagram.java
@@ -2,7 +2,10 @@ package ru.spbau.bioinf.tagfinder;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import ru.spbau.bioinf.tagfinder.util.ReaderUtil;
@@ -10,11 +13,16 @@ import ru.spbau.bioinf.tagfinder.util.ReaderUtil;
 public class Diagram {
     public static void main(String[] args) throws Exception {
         Set<Integer> matchedNew = new HashSet<Integer>();
-        BufferedReader in = ReaderUtil.createInputReader(new File("good.txt"));
+        BufferedReader in = ReaderUtil.createInputReader(new File("tag4first.log"));
         String s;
         while ((s = in.readLine())!= null) {
-            if (s.startsWith(" ")) {
-                matchedNew.add(Integer.parseInt(s.substring(1, s.indexOf("!"))));
+            if (!s.startsWith("#")) {
+                String[] data = s.split(" ");
+                if (data.length > 1) {
+                    if (Double.parseDouble(data[2]) < 0.0024) {
+                        matchedNew.add(Integer.parseInt(data[0]));
+                    }
+                }
             }
         }
         Configuration conf = new Configuration(args);
@@ -27,11 +35,17 @@ public class Diagram {
         int total = 0;
         int tagOnly = 0;
         int msOnly = 0;
-        for (int scanId : msAlignResults.keySet()) {
+        List<Integer> keys = new ArrayList<Integer> ();
+        keys.addAll(msAlignResults.keySet());
+        Collections.sort(keys);
+        for (int scanId : keys) {
             total++;
             Scan scan = scans.get(scanId);
             boolean isNew = matchedNew.contains(scanId);
             boolean isOld = evalues.get(scanId) < 0.0024;
+            if (isOld) {
+                //System.out.println(scanId);
+            }
             if (isNew) {
                 tag++;
             }
@@ -56,6 +70,5 @@ public class Diagram {
         System.out.println("both = " + both);
         System.out.println("msOnly = " + msOnly);
         System.out.println("tagOnly = " + tagOnly);
-
     }
 }
